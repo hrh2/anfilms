50a7303cfad5a586d4586e08676bf637373a36b8
hrh <gakundohope5@gmail.com>
2024-05-08 13:15:33 +0200

    update on ids

--- src/data/data.js
+++ src/data/data.js
@@ -22,8 +22,8 @@ export const adminNavigationLinks = [
 ];
 
 export const homeNavigationLinks = [
-    { id: 1, title: 'Home', link: '/', icon : <HiHomeModern size={25}/> },
-    { id: 2, title: 'Videos',link: '/videos', icon : <MdVideoLibrary size={27}/> },
+    { id: 783, title: 'Home', link: '/', icon : <HiHomeModern size={25}/> },
+    { id: 93, title: 'Videos',link: '/videos', icon : <MdVideoLibrary size={27}/> },
     { id: 272, title: 'Movies',link: '/movies', icon : <MdLocalMovies   size={27}/> },
     { id: 233, title: 'CGIs',link: '/cgi', icon : <GiAnimalHide  size={27}/> },
     { id: 344, title: 'Music',link: '/musics', icon : <MdLibraryMusic size={27}/> },
@@ -218,7 +218,7 @@ export const teamates = [
         twitter: "https://twitter.com/example_alpha",
         instagram: "https://www.instagram.com/example_alpha"
       },
-      svg: <svg className="absolute text-white !text-gray-700 left-0 bottom-0" xmlns="http://www.w3.org/2000/svg"
+      svg: <svg className="absolute text-gray-700 left-0 bottom-0" xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 1440 320">
       <path fill="currentColor"
         d="M0,288L48,272C96,256,192,224,288,197.3C384,171,480,149,576,165.3C672,181,768,235,864,250.7C960,267,1056,245,1152,250.7C1248,256,1344,288,1392,304L1440,320L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z">
@@ -235,7 +235,7 @@ export const teamates = [
         twitter: "https://twitter.com/example_beta",
         instagram: "https://www.instagram.com/example_beta"
       },
-      svg: <svg className="absolute text-white !text-gray-700  left-0 bottom-0" xmlns="http://www.w3.org/2000/svg"
+      svg: <svg className="absolute text-gray-700  left-0 bottom-0" xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 1440 320">
       <path fill="currentColor"
         d="M0,96L48,128C96,160,192,224,288,240C384,256,480,224,576,213.3C672,203,768,213,864,202.7C960,192,1056,160,1152,128C1248,96,1344,64,1392,48L1440,32L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z">
@@ -252,7 +252,7 @@ export const teamates = [
         twitter: "https://twitter.com/example_gamma",
         instagram: "https://www.instagram.com/example_gamma"
       },
-      svg: <svg className="absolute text-white !text-gray-700 left-0 bottom-0" xmlns="http://www.w3.org/2000/svg"
+      svg: <svg className="absolute text-gray-700 left-0 bottom-0" xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 1440 320">
       <path fill="currentColor"
         d="M0,288L48,256C96,224,192,160,288,160C384,160,480,224,576,213.3C672,203,768,117,864,85.3C960,53,1056,75,1152,69.3C1248,64,1344,32,1392,16L1440,0L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z">
