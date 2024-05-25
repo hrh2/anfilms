ae92d4f74f2ec5b64380c397f8365c9a6e97ecf3
Hirw Hope <gakundohope5@gmail.com>
2024-05-25 13:46:08 +0200

    server updates

--- src/data/servers.js
+++ src/data/servers.js
@@ -1,30 +1,30 @@
-// export const streamServerUrl ={
-// free:"https://rebavid.ue.r.appspot.com",
-// watching:"https://rebavid.ue.r.appspot.com",
-// image:"https://rebavid.ue.r.appspot.com",
-// defaultPlayer:"https://rebavid.ue.r.appspot.com",
-// upload:"https://rebavid.ue.r.appspot.com"
-
-// }
-//
-// export const defaultServerUrl ={
-//     activities:"https://rebavid.ue.r.appspot.com",
-//     authentication:"https://rebavid.ue.r.appspot.com",
-//     accountActivities:"https://rebavid.ue.r.appspot.com",
-// }
 export const streamServerUrl ={
-    free:"http://localhost:5000",
-    watching:"http://localhost:5000",
-    image:"http://localhost:5000",
-    defaultPlayer:"http://localhost:5000",
-    upload:"http://localhost:5000"
+free:"https://rebavid.ue.r.appspot.com",
+watching:"https://rebavid.ue.r.appspot.com",
+image:"https://rebavid.ue.r.appspot.com",
+defaultPlayer:"https://rebavid.ue.r.appspot.com",
+upload:"https://rebavid.ue.r.appspot.com"
+
 }
 
 export const defaultServerUrl ={
-    activities:"http://localhost:5000",
-    authentication:"http://localhost:5000",
-    accountActivities:"http://localhost:5000",
+    activities:"https://rebavid.ue.r.appspot.com",
+    authentication:"https://rebavid.ue.r.appspot.com",
+    accountActivities:"https://rebavid.ue.r.appspot.com",
 }
+// export const streamServerUrl ={
+//     free:"http://localhost:5000",
+//     watching:"http://localhost:5000",
+//     image:"http://localhost:5000",
+//     defaultPlayer:"http://localhost:5000",
+//     upload:"http://localhost:5000"
+// }
+//
+// export const defaultServerUrl ={
+//     activities:"http://localhost:5000",
+//     authentication:"http://localhost:5000",
+//     accountActivities:"http://localhost:5000",
+// }
 
 export const admin ={}
 
